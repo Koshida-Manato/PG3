24840182e7c6785a7f61b04a6744f9e6f9b52aa1
KoshidaManato <k022g0129@g.neec.ac.jp>
2023-10-17 10:35:14 +0900

    01確認課題完了

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
-﻿
+﻿#include <stdio.h>
+
+template<typename Type> Type Min(Type a, Type b) {
+	if (a > b) {
+		return static_cast<Type>(b);
+	}
+
+	if (a < b) {
+		return static_cast<Type>(a);
+	}
+}
+
+template<> char Min<char>(char a, char b) {
+	printf("数字以外は代入できません");
+	return 0;
+}
+
+int main() {
+
+	char a = 'a';
+	char b = 'b';
+
+	printf("%d\n", Min<int>(100, 90));
+	printf("%f\n", Min<float>(10.0f, 90.0f));
+	printf("%f\n", Min<double>(15.0f, 9.0f));
+	// printf("%c\n", Min<char>(a,b));
+	Min<char>(a, b);
+
+	return 0;
+}
