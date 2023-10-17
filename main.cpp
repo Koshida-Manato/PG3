2e037feab842ecf6dd85024bdfb31e6a9556ef50
KoshidaManato <k022g0129@g.neec.ac.jp>
2023-10-17 10:41:17 +0900

    02確認課題完了

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
-﻿
+﻿#include <stdio.h>
+
+int Recursive(int h, int j, int m) {
+
+	if (h <= 0) {
+		return j;
+	}
+
+	return Recursive(h - 1, j += m, m * 2 - 50);
+}
+
+int main() {
+
+	int hour = 4;
+	int money = 100;
+	int total = 0;
+	int normal = 1072;
+
+	int result;
+
+	result = Recursive(hour, total, money);
+
+	printf("3時間働いた一般賃金体系 : %d     再帰的な賃金体系 : %d", normal * hour, result);
+
+	return 0;
+}
