bf8f94691e37e5bc42455c787646fbee01711f4c
KoshidaManato <k022g0129@g.neec.ac.jp>
2023-11-14 10:06:53 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,44 +1,12 @@
 ﻿#include <Windows.h>
+#include <functional>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-typedef void (*newType)(int*, int*);
-
-void Lottery(int* select, int* num) {
-
-	printf("サイコロの目は%dでした。\n", *num);
-
-	if (*select == 1) {
-		if (*num == 1 || *num == 3 || *num == 5) {
-			printf("半です");
-		} else {
-			printf("丁です");
-		}
-	}
-
-	if (*select == 2) {
-		if (*num == 0 || *num == 2 || *num == 4 || *num == 6) {
-			printf("丁です");
-		} else {
-			printf("半です");
-		}
-	}
-}
-
-void SetTimeout(newType calc, int second, int select, int num) {
-
-	Sleep(second * 1000);
-
-	calc(&select, &num);
-}
-
 int main() {
-	// ランダム関数
 	srand((unsigned int)time(NULL));
-	newType calc;
 
-	calc = Lottery;
 	int num;
 	int select = 0;
 
@@ -47,7 +15,33 @@ int main() {
 	scanf_s("%d", &select);
 	num = 1 + rand() % 6;
 
-	SetTimeout(calc, 3, select, num);
+	std::function<void(void)> lottery = [=]() {
+		printf("サイコロの目は%dでした。\n", num);
+
+		if (select == 1) {
+			if (num == 1 || num == 3 || num == 5) {
+				printf("半です");
+			} else {
+				printf("丁です");
+			}
+		}
+
+		if (select == 2) {
+			if (num == 0 || num == 2 || num == 4 || num == 6) {
+				printf("丁です");
+			} else {
+				printf("半です");
+			}
+		}
+	};
+
+	std::function<void(int)> setTimeout = [=](int second) {
+		Sleep(second * 1000);
+
+		lottery();
+	};
+
+	setTimeout(3);
 
 	return 0;
 }
